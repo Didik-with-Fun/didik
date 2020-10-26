01f10e456533950f75dc2f1eaae8aa54ce76d7a7
Haddawi <muhamadhaddawi@gmail.com>
2020-10-26 14:03:36 +0700

    Project starter

---
+++ didik/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  didik
+//
+//  Created by Haddawi on 26/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ didik/didikApp.swift
@@ -0,0 +1,17 @@
+//
+//  didikApp.swift
+//  didik
+//
+//  Created by Haddawi on 26/10/20.
+//
+
+import SwiftUI
+
+@main
+struct didikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
