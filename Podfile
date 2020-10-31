b5e00d1f4244e321a4ddaf987360e6c072a0ed6b
Haddawi <muhamadhaddawi@gmail.com>
2020-10-31 17:30:56 +0700

    feat (feat/setup-pods-firebase): firebase & pods setup for didik project

---
+++ Podfile
@@ -0,0 +1,23 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'didik' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for didik
+  pod 'Firebase/Analytics'
+  pod 'Firebase/Auth'
+  pod 'Firebase/Firestore'
+  pod 'FirebaseFirestoreSwift'
+
+  target 'didikTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'didikUITests' do
+    # Pods for testing
+  end
+
+end
--- didik/didikApp.swift
+++ didik/didikApp.swift
@@ -6,12 +6,24 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct didikApp: App {
+    
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        print("--> setting up firebase firestore ..")
+        FirebaseApp.configure()
+        return true
     }
 }
