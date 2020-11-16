07d718026fd3bdc9c4ef2891a60f6fb77e5a89f9
Haddawi <muhamadhaddawi@gmail.com>
2020-11-16 13:11:37 +0700

    fix/dropdown-create-project: fix select dropdown

--- didik/View/CreateProjectView/Topics/TopicMainView.swift
+++ didik/View/CreateProjectView/Topics/TopicMainView.swift
@@ -9,7 +9,12 @@ import Foundation
 import SwiftUI
 
 struct TopicMainView: View {
+    let contents: [Topic] = topicList
+    
+    @State private var isDropdownShowed: Bool = false
     @Binding var contentTopic: Topic
+    
+    @State private var width: CGFloat = 755
     
     var body: some View {
         VStack {
@@ -19,7 +24,55 @@ struct TopicMainView: View {
                     Text("Materi Pembelajaran")
                         .padding(.vertical, 5)
                     
-                    DropdownTopics(isDropdownShowed: false, selected: .constant(contentTopic), width: 755)
+                    //MARK: - Dropdown Topic on Main View
+                    VStack {
+                        Button(action: {
+                            self.isDropdownShowed.toggle()
+                        }) {
+                            HStack (spacing: 10) {
+                                Text(contentTopic.name)
+                                    .frame(width: width * 0.85, alignment: .center)
+                                    .foregroundColor(.black)
+                                    .padding(.vertical, 15)
+                                    .padding(.trailing, 10)
+                                
+                                Image(systemName: "chevron.down")
+                                    .frame(width: width * 0.15, alignment: .center)
+                                    .padding(.trailing, 10)
+                                    .foregroundColor(.black)
+                            }
+                        }
+                        .foregroundColor(Color.Didik.GreyMedium)
+                        .background(Color.Didik.GreyMedium)
+                        .padding(.vertical, 0)
+                        .cornerRadius(15)
+                        .overlay(
+                            VStack{
+                                if isDropdownShowed {
+                                    VStack {
+                                        ForEach(0..<self.contents.count) {
+                                            index in
+                                            Button(action: {
+                                                self.isDropdownShowed.toggle()
+                                                self.contentTopic = contents[index]
+
+                                                // store subject pick and prepare POST
+                                            }) {
+                                                Text(contents[index].name)
+                                                    .foregroundColor(.black)
+                                                    .font(.system(size: 17, weight: .bold, design: .default))
+                                                    .padding(.vertical, 10)
+                                                    .frame(width: width, alignment: .center)
+                                            }
+                                        }
+                                    }
+                                    .background(Color.white)
+                                    .cornerRadius(10)
+                                    .shadow(radius: 5)
+                                }
+                            }, alignment: .topLeading
+                        )
+                    }
                 }
             }
             
